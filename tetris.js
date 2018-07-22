7748fcecf7595c5e4cc87231380a3cf37b022138
mrandrewlopez <andrewlopezcodes@gmail.com>
2018-07-22 02:32:08 -0400

    fixed

--- tetris.js
+++ tetris.js
@@ -1,7 +1,5 @@
 const canvas = document.getElementById('tetris');
-cibst context = canvas.getContext('2d');
+const context = canvas.getContext('2d');
 
 context.fillStyle = '#000';
 context.fillRect(0, 0,  canvas.width, canvas.height);
-
-happpy

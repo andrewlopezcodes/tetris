77d8b4dcba262753da0766af650a71390f5e2a98
mrandrewlopez <andrewlopezcodes@gmail.com>
2018-07-22 02:29:15 -0400

    nah

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 const canvas = document.getElementById('tetris');
 cibst context = canvas.getContext('2d');
+
+context.fillStyle = '#000';
+context.fillRect(0, 0,  canvas.width, canvas.height);
+
+happpy

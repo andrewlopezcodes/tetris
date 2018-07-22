c429b05ab7a0d5bd5e623f2673ecf778a1c0aff1
mrandrewlopez <andrewlopezcodes@gmail.com>
2018-07-22 13:15:14 -0400

    created areana

--- tetris.js
+++ tetris.js
@@ -10,6 +10,14 @@ const matrix = [
   [0, 1, 0]
 ];
 
+function createMatrix(w, h){
+  const matrix = [];
+  while(h--) {
+    matrix.push(new Array(w).fill(0));
+  }
+  return matrix;
+}
+
 function draw(){
   context.fillStyle = '#000';
   context.fillRect(0, 0,  canvas.width, canvas.height);
@@ -25,6 +33,16 @@ function drawMatrix(matrix, offset){
         context.fillRect( x + offset.x,
                           y+offset.y,
                           1, 1);
+      }
+    });
+  });
+}
+
+function merge(arena, player){
+  player.matrix.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if(value !== 0){
+        arena[y + player.pos.y][x + player.pos.x] = value;
       }
     });
   });
@@ -50,6 +68,9 @@ function update(time = 0){
   draw();
   requestAnimationFrame(update);
 }
+
+const arena = createMatrix(12, 20);
+
 
 const player = {
   pos: {x: 5, y: 5},

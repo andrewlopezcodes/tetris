82797050da1ffd5c1962ca7f09ddb690835b667d
mrandrewlopez <andrewlopezcodes@gmail.com>
2018-07-23 00:34:49 -0400

    create random pieces

--- tetris.js
+++ tetris.js
@@ -4,11 +4,6 @@ const context = canvas.getContext('2d');
 
 context.scale(20, 20);
 
-const matrix = [
-  [0, 0, 0],
-  [1, 1, 1],
-  [0, 1, 0]
-];
 
 function collide(arena, player){
   const [m, o] = [player.matrix, player.pos];
@@ -30,10 +25,57 @@ function createMatrix(w, h){
   return matrix;
 }
 
+function createPiece(type){
+  if(type === 'T'){
+    return [
+      [0, 0, 0],
+      [1, 1, 1],
+      [0, 1, 0]
+    ];
+  } else if (type === 'O'){
+    return [
+      [1, 1],
+      [1, 1]
+    ];
+  }  else if (type === 'L'){
+    return [
+      [0, 0, 0],
+      [0, 1, 0],
+      [0, 1, 1]
+    ];
+  } else if (type === 'J'){
+    return [
+      [0, 0, 0],
+      [0, 1, 0],
+      [1, 1, 0]
+    ];
+  } else if (type === 'I'){
+    return [
+      [0, 1, 0, 0],
+      [0, 1, 0, 0],
+      [0, 1, 0, 0],
+      [0, 1, 0, 0]
+    ];
+  } else if (type === 'S'){
+    return [
+      [0, 1, 1],
+      [1, 1, 0],
+      [0, 0, 0]
+    ];
+  } else if (type === 'Z'){
+    return [
+      [1, 1, 0],
+      [0, 1, 1],
+      [0, 0, 0]
+    ];
+  }
+}
+
 function draw(){
   context.fillStyle = '#000';
   context.fillRect(0, 0,  canvas.width, canvas.height);
 
+  drawMatrix(arena, {x: 0, y: 0});
   drawMatrix(player.matrix, player.pos);
 }
 
@@ -62,8 +104,56 @@ function merge(arena, player){
 
 function playerDrop(){
   player.pos.y++;
+  if(collide(arena, player)){
+    player.pos.y--;
+    merge(arena, player);
+    playerReset();
+  }
   dropCounter = 0;
 }
+
+function playerMove(dir){
+  player.pos.x += dir;
+  if(collide(arena, player)){
+    player.pos.x -= dir;
+  }
+}
+
+function playerReset(){
+  const pieces = 'ILJOTSZ';
+  player.matrix = createPiece(pieces[pieces.length* Math.random() | 0]);
+  player.pos.y = 0;
+  player.pos.x = (arena[0].length /2 | 0) - (player.matrix[0].length / 2 | 0);
+}
+
+function playerRotate(dir){
+  const pos = player.pos.x;
+  let offset = 1;
+  rotate(player.matrix, dir);
+  while (collide(arena, player)){
+    player.pos.x +=offset;
+    offset = - (offset + (offset > 0 ? 1: -1));
+    if (offset > player.matrix[0].length){
+      rotate(player.matrix, -dir);
+      player.pos.x = pos;
+      return;
+    }
+  }
+}
+
+function rotate(matrix, dir){
+  for(let y =0; y < matrix.length; ++y){
+    for (let x =0; x<y; ++x){
+      [matrix[x][y], matrix[y][x]] = [matrix[y][x],  matrix[x][y]];
+    }
+  }
+  if (dir > 0){
+    matrix.forEach(row => row.reverse());
+  } else{
+    matrix.reverse();
+  }
+}
+
 
 let dropCounter = 0;
 let dropInterval = 1000;
@@ -86,16 +176,20 @@ const arena = createMatrix(12, 20);
 
 const player = {
   pos: {x: 5, y: 5},
-  matrix: matrix,
+  matrix: createPiece('T'),
 }
 
 document.addEventListener('keydown', event => {
   if (event.keyCode === 37){
-    player.pos.x--;
+    playerMove(-1);
   } else if (event.keyCode === 39){
-    player.pos.x++;
+    playerMove(1);
   } else if (event.keyCode === 40){
     playerDrop();
+  } else if (event.keyCode === 81){
+    playerRotate(-1)
+  } else if (event.keyCode === 87){
+    playerRotate(1)
   }
 });
 
